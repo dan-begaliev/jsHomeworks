d2b75402d68fe706cd640cdcff315f46eed71170
dan-begaliev <begalievvvv@gmail.com>
2023-05-23 18:14:39 -0700

    first

---
+++ dan/fortune-teller/index.js
@@ -0,0 +1,5 @@
+const numberOfChildren = 4;
+const partnerName = "Aelita";
+const geoLocation = "San Francisco";
+const jobTitle = "software engineer";
+console.log ("You will be a"+ jobTitle + "in"+ geoLocation + "and married to "+ partnerName + "with" + numberOfChildren +"kids.")
