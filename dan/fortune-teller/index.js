48f192e75ae830be063e0d4c57d823f49e3ecde6
dan-begaliev <begalievvvv@gmail.com>
2023-05-24 16:58:17 -0700

    fortune

--- dan/fortune-teller/index.js
+++ dan/fortune-teller/index.js
@@ -1,5 +1,5 @@
-const numberOfChildren = 4;
+const numChildren = 4;
 const partnerName = "Aelita";
-const geoLocation = "San Francisco";
+const location = "San Francisco";
 const jobTitle = "software engineer";
-console.log ("You will be a"+ jobTitle + "in"+ geoLocation + "and married to "+ partnerName + "with" + numberOfChildren +"kids.")
+console.log (`You will be a${jobTitle}in${location}and married to ${partnerName}with${numChildren}kids.`)
